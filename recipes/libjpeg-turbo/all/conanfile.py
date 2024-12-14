77f20f450b7b2b434987580dc86799ec84e0b094
Altay Murat <amurat1@fastmail.fm>
2024-12-13 20:07:39 -0500

    updates for dev build

--- recipes/libjpeg-turbo/all/conanfile.py
+++ recipes/libjpeg-turbo/all/conanfile.py
@@ -3,6 +3,7 @@ from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import copy, get, replace_in_file, rm, rmdir
+from conan.tools.files import apply_conandata_patches, export_conandata_patches
 from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 from conan.tools.scm import Version
 import os
@@ -46,6 +47,9 @@ class LibjpegTurboConan(ConanFile):
         "enable12bit": False,
     }
 
+    def export_sources(self):
+        export_conandata_patches(self)
+        
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -132,6 +136,7 @@ class LibjpegTurboConan(ConanFile):
                               "")
 
     def build(self):
+        apply_conandata_patches(self)
         self._patch_sources()
         cmake = CMake(self)
         cmake.configure()
